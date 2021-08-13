839ef4b5aeca7f5ffecf7be5ad0b468e84243a5b
Tsitra <nrb62281@gmail.com>
2021-08-12 20:44:03 -0700

    Removed skill as it is not working well and deprecated. Better structure support.

--- newReport.bat
+++ newReport.bat
@@ -2,4 +2,4 @@
 docker compose build --force-rm --no-cache --pull
 docker compose up
 docker compose down
-docker image rm simc-docker_simc-hiskill simc-docker_simc-lowskill simc-docker_simc-medskill simc-docker_simc-perfectskill
+# docker image rm 
